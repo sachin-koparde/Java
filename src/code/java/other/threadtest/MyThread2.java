56c299553f96108da15b01ddfe56eff3a315b724
Sachin Koparde <sachinkoparde26@gmail.com>
2021-06-24 15:24:52 +0530

    test updated

--- src/code/java/other/threadtest/MyThread2.java
+++ src/code/java/other/threadtest/MyThread2.java
@@ -1,12 +1,22 @@
 package code.java.other.threadtest;
 
-public class MyThread2 {
+public class MyThread2 implements Runnable{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
 
-        MyThread myThread = new MyThread();
-        myThread.start();
+        MyThread2 myThread = new MyThread2();
+        myThread.run();
+
+        Thread thread = new Thread("newThread");
+        Thread.sleep(3000);
+        thread.start();
+
+        System.out.println(Thread.currentThread().getName() + " is alive?" + thread.isAlive());
 
     }
 
+    @Override
+    public void run() {
+        System.out.println("Running...");
+    }
 }
